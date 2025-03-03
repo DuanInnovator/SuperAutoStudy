9fffad0040ec384689bd9f4d38d08f5da9b0ca06
DUAN <2949749043@qq.com>
2025-03-03 22:35:10 +0800

    [feat] 进度刷新

---
+++ src/main/java/com/wk/constant/FlushProgressConstant.java
@@ -0,0 +1,16 @@
+package com.wk.constant;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 进度刷新常量
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/3/1
+ * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
+ **/
+public class FlushProgressConstant {
+    /**
+     * 最大刷新进度
+     */
+    public static final int MAX_PROGRESS = 5;
+}
+
