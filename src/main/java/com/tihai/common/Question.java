7c8b9c57278c83fef8a97e633727e12a22ae75bd
DUAN <2949749043@qq.com>
2025-04-21 15:39:51 +0800

    [feat] 题目实体类

---
+++ src/main/java/com/tihai/common/Question.java
@@ -0,0 +1,30 @@
+package com.tihai.common;
+
+import lombok.Data;
+
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 题目实体
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/2/26
+ * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
+ **/
+@Data
+public class Question {
+    private static int count = 0;
+    private final int id;
+    private final String title;
+    private final String options;
+    private final String type;
+    private String answer;
+
+    public Question(String title, String options, String type) {
+        this.id = ++count;
+        this.title = title;
+        this.options = options;
+        this.type = type;
+        this.answer = "";
+    }
+}
+
