04fd404808c0b4bf18d3d6ae5588ac6d217b106b
DUAN <2949749043@qq.com>
2025-04-21 16:07:54 +0800

    [feat] sql

---
+++ sql/th_wk_user.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : 8.136.123.221
+ Source Server Type    : MySQL
+ Source Server Version : 50744 (5.7.44)
+ Source Host           : 8.136.123.221:3306
+ Source Schema         : tihai
+
+ Target Server Type    : MySQL
+ Target Server Version : 50744 (5.7.44)
+ File Encoding         : 65001
+
+ Date: 21/04/2025 16:07:27
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for th_wk_user
+-- ----------------------------
+DROP TABLE IF EXISTS `th_wk_user`;
+CREATE TABLE `th_wk_user`  (
+  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `school_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学校名称',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `fid` bigint(20) NULL DEFAULT NULL COMMENT '学校id',
+  `cookies` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '用户当前cookie'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '网课-用户信息表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
