e688e1e00e8cbfcc9f898f84bbd2319ac9bb7384
DUAN <2949749043@qq.com>
2025-05-17 17:52:10 +0800

    ✨ [feat] 题目类型常量

---
+++ src/main/java/com/tihai/constant/QuestionTypeConstant.java
@@ -0,0 +1,18 @@
+package com.tihai.constant;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 题目类型常量
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/4/29
+ * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
+ **/
+public class QuestionTypeConstant {
+
+    public static final int SINGLE_CHOICE =0 ;
+    public static final int MULTIPLE_CHOICE =1 ;
+    public static final int FILL_IN =2 ;
+    public static final int JUDGMENT =3 ;
+    public static final int SHORT_ANSWER =4 ;
+}
+
