2429a122a9f568a017c0f38024230f97180f7bb3
DUAN <2949749043@qq.com>
2025-05-17 17:51:35 +0800

    ✨ [feat] 查题响应VO

---
+++ src/main/java/com/tihai/vo/QueryVO.java
@@ -0,0 +1,18 @@
+package com.tihai.vo;
+
+import lombok.Data;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 查题响应VO
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/5/17
+ * @Link : <a href="https://github.com/DuanInnovator/SuperTiKu">...</a>
+ **/
+@Data
+public class QueryVO {
+    private Integer code;
+    private String data;
+    private String msg;
+}
+
