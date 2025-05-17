0b2edae2c95bba17c1af0a1b137841664cd7925e
DUAN <2949749043@qq.com>
2025-05-17 17:51:26 +0800

    ✨ [feat] 响应常量

---
+++ src/main/java/com/tihai/constant/QueryResponseConstant.java
@@ -0,0 +1,13 @@
+package com.tihai.constant;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 查题响应常量
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/5/17
+ * @Link : <a href="https://github.com/DuanInnovator/SuperTiKu">...</a>
+ **/
+public class QueryResponseConstant {
+    public static final Integer SUCCESS_CODE = 200;
+}
+
