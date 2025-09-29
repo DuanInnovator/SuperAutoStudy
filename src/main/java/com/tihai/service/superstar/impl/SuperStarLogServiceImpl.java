c3d8c0c05900f6a52afcb98353ffc42beab446e5
DuanInnovator <2949749043@qq.com>
2025-09-29 14:19:40 +0800

    fix 优化日志服务实现类

--- src/main/java/com/tihai/service/superstar/impl/SuperStarLogServiceImpl.java
+++ src/main/java/com/tihai/service/superstar/impl/SuperStarLogServiceImpl.java
@@ -1,11 +1,19 @@
 package com.tihai.service.superstar.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tihai.domain.chaoxing.SuperStarLog;
+import com.tihai.domain.chaoxing.SuperStarTask;
+import com.tihai.enums.WkTaskStatusEnum;
 import com.tihai.mapper.SuperStarLogMapper;
 import com.tihai.service.superstar.SuperStarLogService;
+import ma.glasnost.orika.MapperFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @Copyright : DuanInnovator
@@ -15,10 +23,31 @@ import org.springframework.stereotype.Service;
  * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
  **/
 @Service
+@SuppressWarnings("all")
 public class SuperStarLogServiceImpl extends ServiceImpl<SuperStarLogMapper, SuperStarLog> implements SuperStarLogService {
+
+    @Autowired
+    private MapperFacade mapperFacade;
+
+
+    /**
+     * 创建新的日志
+     *
+     * @param task 任务
+     * @return 日志
+     */
+    public SuperStarLog createNewLog(SuperStarTask task) {
+        SuperStarLog log = mapperFacade.map(task, SuperStarLog.class);
+        log.setStartTime(LocalDateTime.now());
+        log.setStatus(WkTaskStatusEnum.PROCESSING.getCode());
+        log.setId(IdWorker.getId());
+        return log;
+    }
+
 
     /**
      * 保存日志
+     *
      * @param superStartLog 日志信息
      */
     @Override
@@ -28,28 +57,37 @@ public class SuperStarLogServiceImpl extends ServiceImpl<SuperStarLogMapper, Sup
     }
 
     /**
-     * 更新日志
+     * 批量保存日志
+     *
      * @param superStartLog 日志信息
      */
     @Override
-    public void updateLog(SuperStarLog superStartLog) {
-        this.updateById(superStartLog);
+    public void batchSaveLog(List<SuperStarLog> superStartLog) {
+        this.saveOrUpdateBatch(superStartLog);
     }
 
+
     /**
-     * 根据账号和课程名获取最新的一条日志
-     * @param account 账号
-     * @param courseName 课程
-     * @return 最新一条日志
+     * 根据账号，课程获取最新一条日志
+     *
+     * @param account    账号
+     * @param courseName 课程名称
+     * @param courseId   课程ID
      */
     @Override
-    public SuperStarLog getLatestLogByLoginAccount(String account, String courseName) {
+    public SuperStarLog getLatestLogByLoginAccount(String account, String courseName, String courseId) {
         LambdaQueryWrapper<SuperStarLog> superStartLogLambdaQueryWrapper = new LambdaQueryWrapper<>();
         superStartLogLambdaQueryWrapper.eq(SuperStarLog::getLoginAccount, account);
-        superStartLogLambdaQueryWrapper.eq(SuperStarLog::getCourseName, courseName);
-        superStartLogLambdaQueryWrapper.orderByDesc(SuperStarLog::getId).last("limit 1");
+        if (courseId != null) {
+            superStartLogLambdaQueryWrapper.eq(SuperStarLog::getCourseId, courseId);
+        } else if (courseName != null) {
+            superStartLogLambdaQueryWrapper.eq(SuperStarLog::getCourseName, courseName);
+        }
+        superStartLogLambdaQueryWrapper.eq(SuperStarLog::getIsDelete, 0);
+        superStartLogLambdaQueryWrapper.orderByDesc(SuperStarLog::getEndTime)
+                .last("limit 1");
 
-        return this.getOne(superStartLogLambdaQueryWrapper,false);
+        return this.getOne(superStartLogLambdaQueryWrapper, false);
 
     }
 
