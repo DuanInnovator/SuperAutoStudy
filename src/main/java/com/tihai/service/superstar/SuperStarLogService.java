36995ea69cd933489b3d04dc5cf04b743eabe9b7
DuanInnovator <2949749043@qq.com>
2025-09-29 14:18:58 +0800

    fix 优化日志服务接口

--- src/main/java/com/tihai/service/superstar/SuperStarLogService.java
+++ src/main/java/com/tihai/service/superstar/SuperStarLogService.java
@@ -2,6 +2,8 @@ package com.tihai.service.superstar;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.tihai.domain.chaoxing.SuperStarLog;
+import com.tihai.domain.chaoxing.SuperStarTask;
+import java.util.List;
 
 /**
  * @Copyright : DuanInnovator
@@ -12,23 +14,37 @@ import com.tihai.domain.chaoxing.SuperStarLog;
  **/
 public interface SuperStarLogService extends IService<SuperStarLog> {
 
+
+    /**
+     * 创建新的日志
+     *
+     * @param task 任务
+     * @return 日志
+     */
+    SuperStarLog createNewLog(SuperStarTask task);
+
+
     /**
      * 保存日志
+     *
      * @param superStartLog 日志信息
      */
     void saveLog(SuperStarLog superStartLog);
 
     /**
-     * 更新日志
+     * 批量保存日志
+     *
      * @param superStartLog 日志信息
      */
-    void updateLog(SuperStarLog superStartLog);
+    void batchSaveLog(List<SuperStarLog> superStartLog);
 
     /**
      * 根据账号，课程获取最新一条日志
-     * @param account 账号
-     * @param courseName 课程
+     *
+     * @param account    账号
+     * @param courseName 课程名称
+     * @param courseId   课程ID
      */
-    SuperStarLog getLatestLogByLoginAccount(String account, String courseName);
+    SuperStarLog getLatestLogByLoginAccount(String account, String courseName, String courseId);
 }
 
