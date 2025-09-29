4016fb3e7352b474fd5981e5c4021b2d7fe76a56
DuanInnovator <2949749043@qq.com>
2025-09-29 14:15:23 +0800

    feat 响应码

---
+++ src/main/java/com/tihai/common/ResponseCode.java
@@ -0,0 +1,27 @@
+package com.tihai.common;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 响应码
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/8/3
+ * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
+ **/
+@SuppressWarnings("all")
+public class ResponseCode {
+
+    public static final int REDIRECT = 302;
+
+    public static final int TWOZEROTWO = 202;
+
+    public static final int SUCCESS = 200;
+
+    public static final int FAIL = 500;
+
+    public static final int NOT_FOUND = 404;
+
+    public static final int UNAUTHORIZED = 401;
+
+    public static final int FORBIDDEN = 403;
+}
+
