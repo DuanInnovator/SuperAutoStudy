642017580cbcb6bed6da1ac9fb5020c1d2fe2b5c
DuanInnovator <2949749043@qq.com>
2025-09-29 14:20:29 +0800

    feat 超星学习通任务Mapper

---
+++ src/main/java/com/tihai/mapper/SuperStarTaskMapper.java
@@ -0,0 +1,18 @@
+package com.tihai.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tihai.domain.chaoxing.SuperStarTask;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Copyright : DuanInnovator
+ * @Description : 超星学习通任务Mapper
+ * @Author : DuanInnovator
+ * @CreateTime : 2025/5/20
+ * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
+ **/
+@Mapper
+public interface SuperStarTaskMapper extends BaseMapper<SuperStarTask> {
+}
+
