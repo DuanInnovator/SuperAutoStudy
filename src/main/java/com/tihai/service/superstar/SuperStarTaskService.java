2de4d9baf1c249004d2847bbae0872c591613d68
DuanInnovator <2949749043@qq.com>
2025-09-29 14:21:33 +0800

    fix 优化超星学习通任务服务接口

--- src/main/java/com/tihai/service/superstar/SuperStarTaskService.java
+++ src/main/java/com/tihai/service/superstar/SuperStarTaskService.java
@@ -6,10 +6,11 @@ import com.tihai.domain.chaoxing.SuperStarTask;
 import com.tihai.dubbo.dto.CourseSubmitTaskDTO;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @Copyright : DuanInnovator
- * @Description : 超星学习通=服务接口
+ * @Description : 超星学习通任务服务接口
  * @Author : DuanInnovator
  * @CreateTime : 2025/3/1
  * @Link : <a href="https://github.com/DuanInnovator/SuperAutotudy">...</a>
@@ -28,6 +29,12 @@ public interface SuperStarTaskService extends IService<SuperStarTask> {
     SuperStarTask getSuperStarTask(String loginAccount, String courseId);
 
     /**
+     * 获取所有正在处理的任务
+     * @return
+     */
+    List<SuperStarTask> getProcessingTasks();
+
+    /**
      * 根据课程名称查询超星学习任务
      *
      * @param loginAccount
@@ -35,6 +42,18 @@ public interface SuperStarTaskService extends IService<SuperStarTask> {
      * @return
      */
     SuperStarTask getSuperStarTaskByCourseName(String loginAccount, String courseName);
+
+    /**
+     * 更新超星学习任务
+     * @param task
+     */
+    void updateSuperStarTask(SuperStarTask task);
+
+    /**
+     * 批量更新超星学习任务
+     * @param tasks
+     */
+    void batchUpdateSuperStarTask(List<SuperStarTask> tasks);
 
     /**
      * 添加超星学习任务
